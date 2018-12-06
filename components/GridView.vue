4eb36e9c955c5325becdce2da8cac65be2cf490e
Simon Dellenbach <simon.dellenbach@autodesk.com>
2018-12-06 17:03:05 +0100

    Add GridView component
    Experimenting a bit with Vue and SCSS, trying to get it to scoll horizontally only (not working yet).

---
+++ components/GridView.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="GridView">
+        <div class="GridCell">
+            Grid Cell 1
+        </div>
+        <div class="GridCell">
+            Grid Cell 2
+        </div>
+        <div class="GridCell">
+            Grid Cell 3
+        </div>
+        <div class="GridCell">
+            Grid Cell 4
+        </div>
+        <div class="GridCell">
+            Grid Cell 5
+        </div>
+        <div class="GridCell">
+            Grid Cell 6
+        </div>
+        <div class="GridCell">
+            Grid Cell 7
+        </div>
+        <div class="GridCell">
+            Grid Cell 8
+        </div>
+        <div class="GridCell">
+            Grid Cell 9
+        </div>
+        <div class="GridCell">
+            Grid Cell 10
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    $gridSpacing: 10px;
+
+    .GridView {
+        height: 400px + 2 * $gridSpacing;
+        max-height: 400px + 2 * $gridSpacing;
+        width: auto;
+        overflow: auto;
+        overflow-y: hidden;
+        display: grid;
+        grid-template-columns: 200px 200px 200px;
+        grid-template-rows: top 200px bottom 200px;
+        grid-column-gap: $gridSpacing;
+        grid-row-gap: $gridSpacing;
+
+        .GridCell {
+            width: 200px;
+            height: 200px;
+            background-color: pink;
+            border-width: 0px;
+            border-color: black;
+            border-radius: 10px;
+        }
+    }
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,17 @@
 <template>
   <section class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        joybox
-      </h1>
-      <h2 class="subtitle">
-        A kid-friendly touch-based media library player on the Raspberry Pi
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <GridView />
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
+import GridView from '~/components/GridView.vue';
 
 export default {
     components: {
-        Logo,
+        GridView,
     },
 };
 </script>
@@ -48,25 +26,4 @@ export default {
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
