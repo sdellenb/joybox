ee9acb44db8a29e9f7be7c3bd5a9d141667a570b
Simon Dellenbach <sdellenb@gmail.com>
2018-12-26 15:36:12 +0100

    Add my own fork of play-sound-mplayer that can kill process trees.
    Since I have mplayer installed on Windows with scoop, the command is executing the shim that then runs the player.
    Killing the shim only did leave the player run detached until the track finished.
    
    Also use some of the flags do determine whether playback is ongoing.

--- server/utils/mPlayer.js
+++ server/utils/mPlayer.js
@@ -9,7 +9,6 @@ module.exports = class MPlayer extends BasePlayer {
     constructor() {
         super();
         this.currentlyPlayingTrack = null;
-        this.isPaused = false;
     }
 
     async startPlayback(track) {
@@ -19,18 +18,24 @@ module.exports = class MPlayer extends BasePlayer {
             return;
         }
 
-        // If we're still on the same file, continue playing.
-        if (this.currentlyPlayingTrack && this.currentlyPlayingTrack.path === filepath) {
-            return this.pausePlayback(track);
+        if (mplayer.isPlaying || mplayer.isPaused) {
+            // If we're still on the same file, continue playing.
+            if (this.currentlyPlayingTrack && this.currentlyPlayingTrack.path === filepath) {
+                return this.pausePlayback(track);
+            }
+            else {
+                await this.stopPlayback(this.currentlyPlayingTrack);
+            }
         }
+
         this.currentlyPlayingTrack = track;
 
         const playbackFinishedPromise = new Promise(function(resolve, reject) { // eslint-disable-line no-unused-vars
             mplayer.once('start', () => {
-                debug('MPlayer is starting playback.');
+                debug(`MPlayer is starting playback of ${JSON.stringify(track)}.`);
             });
             mplayer.once('end', () => {
-                debug('MPlayer finished playback.');
+                debug(`MPlayer finished playback of ${JSON.stringify(track)}.`);
                 resolve(track);
             });
             mplayer.play(filepath);
@@ -41,20 +46,18 @@ module.exports = class MPlayer extends BasePlayer {
     async pausePlayback(track) { // eslint-disable-line no-unused-vars
         const that = this;
         const playbackPausedPromise = new Promise(function(resolve, reject) { // eslint-disable-line no-unused-vars
-            if (that.isPaused) {
-                debug('MPlayer is resuming playback.');
+            if (mplayer.isPaused) {
+                debug(`MPlayer is resuming playback of ${JSON.stringify(that.currentlyPlayingTrack)}.`);
                 mplayer.once('resume', () => {
-                    debug('MPlayer resumed playback.');
-                    that.isPaused = false;
+                    debug(`MPlayer resumed playback of ${JSON.stringify(that.currentlyPlayingTrack)}.`);
                     resolve(that.currentlyPlayingTrack);
                 });
                 mplayer.resume();
             }
             else {
-                debug('MPlayer is pausing playback.');
+                debug(`MPlayer is pausing playback of ${JSON.stringify(that.currentlyPlayingTrack)}.`);
                 mplayer.once('pause', () => {
-                    debug('MPlayer paused playback.');
-                    that.isPaused = true;
+                    debug(`MPlayer paused playback of ${JSON.stringify(that.currentlyPlayingTrack)}.`);
                     resolve(that.currentlyPlayingTrack);
                 });
                 mplayer.pause();
@@ -65,12 +68,18 @@ module.exports = class MPlayer extends BasePlayer {
 
     async stopPlayback(track) {
         const playbackStoppedPromise = new Promise(function(resolve, reject) { // eslint-disable-line no-unused-vars
-            debug('MPlayer is stopping playback.');
-            mplayer.once('stop', () => {
-                debug('MPlayer is stopped playback.');
-                resolve(track);
-            });
-            mplayer.stop();
+            if (mplayer.isPlaying || mplayer.isPaused) {
+                debug(`MPlayer is stopping playback of ${JSON.stringify(track)}.`);
+                mplayer.once('stop', () => {
+                    debug(`MPlayer stopped playback of ${JSON.stringify(track)}.`);
+                    resolve(track);
+                });
+                mplayer.stop();
+            }
+            else {
+                reject('MPlayer not running.');
+            }
+
         });
         return playbackStoppedPromise;
     }
